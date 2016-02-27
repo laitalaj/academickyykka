5fe61c8ddccf6b78026bdc93c1935b02bc2255b2
laitalaj <julius.laitala@helsinki.fi>
2016-02-27 19:54:53 +0200

    Aloitettu valikkojen tekeminen

---
+++ academickyykka/src/main/java/org/kyykka/io/GameInitializer.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kyykka.io;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.Semaphore;
+import org.kyykka.logic.Game;
+import org.kyykka.logic.player.AIPlayer;
+import org.kyykka.logic.player.HumanPlayer;
+import org.kyykka.logic.player.Player;
+
+/**
+ *
+ * @author Admin
+ */
+public class GameInitializer implements ActionListener {
+    
+    private Display display;
+    private MenuPanel menu;
+    private Semaphore lock;
+    private Game game;
+
+    public GameInitializer(Display display, MenuPanel menu, Semaphore lock)
+            throws InterruptedException{
+        this.display = display;
+        this.menu = menu;
+        this.menu.getStartButton().addActionListener(this);
+        this.lock = lock;
+        this.lock.acquire();
+    }
+    
+    private void addPlayer(String player, Input input, 
+            CoordinateTranslator translator, boolean homeTeam){
+        switch(player){
+            case "Human":{
+                this.game.addPlayer(new HumanPlayer(input, translator));
+                break;
+            }
+            case "AI": {
+                this.game.addPlayer(new AIPlayer(game, homeTeam));
+                break;
+            }
+        }
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String player1 = this.menu.getHomePlayerChoice().getActionCommand();
+        String player2 = this.menu.getHomePlayerChoice().getActionCommand();
+        this.game = new Game();
+        Input input = new Input(this.display);
+        //TODO: Fit size!
+        CoordinateTranslator translator = new CoordinateTranslator(game, 1200, 700);
+        addPlayer(player1, input, translator, true);
+        addPlayer(player2, input, translator, false);
+        
+    }
+    
+}
